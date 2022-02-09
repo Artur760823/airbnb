7ece2a4a08ed9be302007d506c97588f12e35530
Artur <artur.zajaczkowski76@gmail.com>
2022-02-09 16:31:31 +0100

    Add rating to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,6 +5,9 @@ function Card() {
   return (
     <div className='card'>
       <div className='card__img'></div>
+      <span className='card__stats'>4.5</span>
+      <span className='card__star'>(6) *</span>
+      <span>USA</span>
       <p className='card__description'>Rap session with Eminem</p>
       <p className='card__costs'>From 200.99$ / person</p>
     </div>
