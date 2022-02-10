35f81326927b1b671e935ee6cf93f56e41910629
Artur <artur.zajaczkowski76@gmail.com>
2022-02-10 09:14:40 +0100

    little changes

--- src/components/Galery.js
+++ src/components/Galery.js
@@ -4,13 +4,15 @@ import './Galery.css';
 function Galery() {
   return (
     <>
-      <div className='main-galery'>
-        <div className='main-galery__img_1'></div>
-        <div className='main-galery__img_2'></div>
-        <div className='main-galery__img_3'></div>
-        <div className='main-galery__img_4'></div>
-        <div className='main-galery__img_5'></div>
-        <div className='main-galery__img_6'></div>
+      <div className='galery-wrapper'>
+        <div className='main-galery'>
+          <div className='main-galery__img_1'></div>
+          <div className='main-galery__img_2'></div>
+          <div className='main-galery__img_3'></div>
+          <div className='main-galery__img_4'></div>
+          <div className='main-galery__img_5'></div>
+          <div className='main-galery__img_6'></div>
+        </div>
       </div>
       <div className='description'>
         <h1 className='description__title'>Amazing experience in your hand...</h1>
